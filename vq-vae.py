e8e304617447564af0de511cc55ec303b4908cdb
Kyle David <kzd0038@tigermail.auburn.edu>
2021-02-03 11:01:30 -0600

    Update vq-vae.py

--- vq-vae.py
+++ vq-vae.py
@@ -49,6 +49,7 @@ train_file = "saccharomycetales_proteomes.fa"
 output_file = "test"
 
 log = open(output_file + "_log.txt", "w")
+log.write(torch.cuda.get_device_name(device))
 log.write("PARAMETERS\n\n")
 log.write("Basic Block\n")
 log.write("kernel size = " + str(kernel_size) + "\n")
