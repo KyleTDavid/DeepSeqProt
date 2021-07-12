84d2fcbcece01385e20731a07d6ec24e93126fd2
Kyle David <kzd0038@tigermail.auburn.edu>
2021-07-12 15:27:30 -0500

    Update validate.py

--- validate.py
+++ validate.py
@@ -12,8 +12,8 @@ results = []
 results.append(["# of categories", len(set(df.Encoding))])
 
 #only look at protein families with at least 2 members, group by family
-v = df['Protein families'].value_counts()
-fams = df[df['Protein families'].isin(v.index[v.gt(1)])]
+v = uniprot_df['Protein families'].value_counts()
+fams = uniprot_df[uniprot_df['Protein families'].isin(v.index[v.gt(1)])]
 group = fams.groupby('Protein families')['Encoding']
 
 #percentage of complete families (all members have the same encoding)
@@ -23,15 +23,20 @@ results.append(["complete families",(len(com[com==True]) / len(com))])
 #family completeness (largest number of members that share a cluster / family size)
 results.append(["family completeness",
                 group.apply(lambda x: x.value_counts().head(1)).sum() / group.size().sum()])
-                
+
+#adjusted mutual information
+uniprot_notnull = uniprot_df[uniprot_df['Protein families'].notnull()]
+results.append(["adjusted mutual information",
+                adjusted_mutual_info_score(uniprot_notnull['Protein families'], uniprot_notnull['Encoding'])])
+
 #run gene ontology enrichment analysis
-import goatools
 # Get http://geneontology.org/ontology/go-basic.obo
+
 from goatools.base import download_go_basic_obo
 obo_fname = download_go_basic_obo()
 
-df['Gene ontology IDs'] = df['Gene ontology IDs'].str.replace(' ','')
-df.drop(['Encoding', 'Organism', 'Protein families', 'n'], axis=1).to_csv("GOA.txt", sep='\t', header=False, index=False)
+uniprot_df['Gene ontology IDs'] = uniprot_df['Gene ontology IDs'].str.replace(' ','')
+uniprot_df.drop(['Encoding', 'Organism', 'Protein families', 'n'], axis=1).to_csv("GOA.txt", sep='\t', header=False, index=False)
 
 from goatools.anno.idtogos_reader import IdToGosReader
 objanno = IdToGosReader("GOA.txt")
@@ -42,7 +47,7 @@ obodag = GODag("go-basic.obo")
 
 from goatools.go_enrichment import GOEnrichmentStudy
 goeaobj = GOEnrichmentStudy(
-        df.Entry, 
+        uniprot_df.Entry,
         ns2assoc, # geneid/GO associations
         obodag, # Ontologies
         propagate_counts = False,
@@ -50,42 +55,41 @@ goeaobj = GOEnrichmentStudy(
         methods = ['fdr_bh']) # default multipletest correction method
 
 gos = []
-#sort by size
-bdf = df[df.n > 1]
-for e in set(bdf['Encoding']):
-#for e in set(encodings.Encoding):
-  goea_results = goeaobj.run_study(list(df[df['Encoding']==e].Entry))
+for e in set(uniprot_df[uniprot_df['n']>=2]['Encoding']):
+  goea_results = goeaobj.run_study(list(uniprot_df[uniprot_df['Encoding']==e].Entry))
   for r in goea_results:
-      if (r.p_fdr_bh < 0.001) & (r.enrichment=='e') :
+      if (r.enrichment=='e'):
         id = r.goterm.id
         name = r.name
         cat = r.goterm.namespace
         members = r.study_items
-        gos.append([id, name, cat, e, members])
+        p = r.p_fdr_bh
+        gos.append([id, name, cat, e, members, p])
 
-godf = pd.DataFrame(gos, columns=['id', 'name', 'category', 'encoding', 'members'])
+godf = pd.DataFrame(gos, columns=['id', 'name', 'category', 'encoding', 'members', 'p'])
+godf['unique?'] = ~godf['name'].duplicated(keep=False)
+godf['member_count'] = godf.members.apply (lambda x: len(x))
+godf['representation'] = godf.apply (lambda row: row.member_count / int(np.unique(uniprot_df[uniprot_df.Encoding==row.encoding]['n'])), axis=1)
+
+godf_sig = godf[godf.p <= 0.01]
 
 #what % of encodings have at least one significant GO?
-results.append(["significant categories",
-                (len(set(godf.encoding)) / len(set(df.Encoding)))])
+results.append(["good categories",
+                (len(set(godf_sig.encoding)) / len(set(uniprot_df.Encoding)))])
 
-members = [item for sublist in godf.members for item in sublist]
+members = [item for sublist in godf_sig.members for item in sublist]
 
 #how many members have at least one significant GO?
 results.append(["GO accuracy",
-                len(set(members)) / len(df[df["Gene ontology IDs"].notnull()].Entry)])
+                len(set(members)) / len(uniprot_df[uniprot_df["Gene ontology IDs"].notnull()].Entry)])
 
 #how many members have at least one significant unique GO?
-v = godf['name'].value_counts()
-uniqgodf = godf[godf['name'].isin(v.index[v.lt(2)])]
+uniqgodf = godf_sig[godf_sig['unique?']==True]
 
 uniqmembers = [item for sublist in uniqgodf.members for item in sublist]
 
 results.append(["unique GO accuracy",
-                len(set(uniqmembers)) / len(df[df["Gene ontology IDs"].notnull()].Entry)])
-                
-results.append(["# of Good Categories",
-				len(set(uniqgodf['encoding']))])
+                len(set(uniqmembers)) / len(uniprot_df[uniprot_df["Gene ontology IDs"].notnull()].Entry)])
 
 resultsdf = pd.DataFrame(results)
 resultsdf.to_csv(sys.argv[2].replace("results", "report"), sep='\t', header=False, index=False)
